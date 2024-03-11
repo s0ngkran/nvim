395321a8757b34ae9215b973f397b75862d32225
s0ngkran <jinnawat8@gmail.com>
2024-03-11 16:15:29 +0700

    tex

--- lua/user/plugins/user.lua
+++ lua/user/plugins/user.lua
@@ -1066,8 +1066,8 @@ return {
       require("dap-vscode-js").setup({
         node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
         -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-        -- debugger_path = "~/bin/vscode-js-debug", 
-        debugger_path = "/Users/walter/bin/vscode-js-debug", 
+        -- debugger_path = "~/bin/vscode-js-debug",
+        debugger_path = "/Users/walter/bin/vscode-js-debug",
         -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
         adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
         -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
@@ -1094,6 +1094,12 @@ return {
           -- }
         }
       end
+    end
+  },
+  {
+    "lervag/vimtex",
+    init = function()
+      -- Use init for configuration, don't use the more common "config".
     end
   }
 
